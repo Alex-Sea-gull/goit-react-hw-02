35bcc41a7442b8e25f4a9569a7c9bf9c0eb6e2bf
Alex Sea <seagullalex81@gmail.com>
2024-12-16 08:56:57 +0200

    add localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Description from "./components/Descriptions/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  // const [feedback, setFeedback] = useState(() => {
+  //   const savedFeedback = JSON.parse(localStorage.getItem("feedback"));
+  // if (savedFeedback && savedFeedback.length) {
+  //     return savedFeedback
+  // }
+  // return []}) не працює якщо savedFeedback об'єкт
+
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    console.log("Сюди приходять дані із localStorage:", savedFeedback);
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+    // if (savedFeedback) {
+    //   return JSON.parse(savedFeedback);
+    // } else {
+    //   return { good: 0, neutral: 0, bad: 0 };
+    // }
   });
+
+  useEffect(() => {
+    if (feedback) {
+      localStorage.setItem("feedback", JSON.stringify(feedback));
+    }
+  }, [feedback]);
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
